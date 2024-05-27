78e3344610f8c87f2918f9ffa9086c418996ff21
Yeskendir Salgara <36730580+sorunokoe@users.noreply.github.com>
2024-05-27 21:14:12 +0200

    Fix user

--- Sources/QaraMedium/Model/QaraMediumUser.swift
+++ Sources/QaraMedium/Model/QaraMediumUser.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 public struct QaraMediumUser: Decodable {
-    var id: String
+    public var id: String
     var username: String
     var name: String
     var url: String
     var imageUrl: String
+    
+    public init(id: String, usernam e: String, name: String, url: String, imageUrl: String) {
+        self.id = id
+        self.username = username
+        self.name = name
+        self.url = url
+        self.imageUrl = imageUrl
+    }
 }
